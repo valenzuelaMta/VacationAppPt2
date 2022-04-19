db2f0ab850b7ab1e58f41c859a147fbbed7b2301
valenzuelaMta <valenzuela.mta@gmail.com>
2022-04-19 16:37:17 -0600

    Changes

--- app/src/main/java/com/example/vacationapp/MainActivity.java
+++ app/src/main/java/com/example/vacationapp/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.vacationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.ParseUser;
+
+import java.security.cert.PKIXRevocationChecker;
 
 public class MainActivity extends AppCompatActivity {
+//    private Button nextActivity1;
+//    private TextView titleMain;
+//    private TextView option1;
+//    private TextView weather1;
+//    private TextView budget1;
+//    private TextView time1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+//        //RelativeLayout container;
+//        titleMain = findViewById(R.id.titleMain);
+//        option1 = findViewById(R.id.option1);
+//        weather1 = findViewById(R.id.weather1);
+//        budget1 = findViewById(R.id.budget1);
+//        time1 = findViewById(R.id.time1);
+//        //nextActivity1 = findViewById(R.id.nextActivity1);
+//        //container = findViewById(R.id.container);
+//        option1.setOnClickListener(new View.OnClickListener() {
+
+
+//            @Override
+//            public void onClick(View view) {
+////                Toast.makeText(MainActivity.this, "Option 1", Toast.LENGTH_SHORT).show();
+////                //openOption1();
+////                Intent i = new Intent(MainActivity.this, Option1.class);
+////                //Intent i = new Intent(MainActivity.this, Option1.class);
+////                startActivity(i);
+//            }
+//        });
+//    }
+//    private void openOption1(){
+//        Intent intent = new Intent(this, Option1.class);
+//        startActivity(intent);
+   }
+
 }
---
+++ app/src/main/java/com/example/vacationapp/Option1.java
@@ -0,0 +1,16 @@
+package com.example.vacationapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//public class Option1 {
+//}
+public class Option1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.option_1);
+    }
+}
